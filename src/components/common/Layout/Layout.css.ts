7933d277f3ca7df3143ccd242fa6e6b0f265c867
frylo0 <frylo.d.ts@gmail.com>
2024-02-07 22:28:52 +0300

    FY-3: Added layout component, useLayout hook and color vars

---
+++ src/components/common/Layout/Layout.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+import { media } from '@/styles/bundle.css';
+
+export const sLayout = style({
+	'@media': {
+		[media.device.desktop]: {
+			paddingInline: '20px',
+		},
+		[media.device.tablet]: {
+			paddingInline: '15px',
+		},
+		[media.device.phone]: {
+			paddingInline: '10px',
+		},
+	},
+});
--- src/styles/global-in-js.css.ts
+++ src/styles/global-in-js.css.ts
@@ -1,10 +1,12 @@
 import { globalStyle } from '@vanilla-extract/css';
 
 import { fonts } from './bundle.css';
+import { colors } from './vars/colors.css';
 
 globalStyle('html', {
 	fontSize: '12px',
-	background: '#222',
+	background: colors.body,
+	color: colors.secondary,
 	fontFamily: fonts.gothamPro,
 	fontWeight: 'normal',
 });
