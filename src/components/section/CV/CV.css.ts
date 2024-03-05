9e59d675b70e91b80a7b8092c791fad4354d292d
frylo0 <frylo.d.ts@gmail.com>
2024-03-05 20:35:12 +0300

    FY-8: Added rocks to background

--- src/components/section/CV/CV.css.ts
+++ src/components/section/CV/CV.css.ts
@@ -11,4 +11,33 @@ export const sSection = style({
 	borderRadius: 100,
 	boxShadow: '0 34px 100px #00000010',
 	position: 'relative',
+	overflow: 'hidden',
+});
+
+export const sContent = style({
+	position: 'relative',
+});
+
+export const sRocky = style({
+	position: 'absolute',
+	height: 'auto',
+});
+
+export const sRocky1 = style({
+	left: -570,
+	top: 400,
+	transform: 'rotate(22deg)',
+	width: 977,
+});
+export const sRocky2 = style({
+	width: 1000,
+	transform: 'rotate(-10deg)',
+	right: -32,
+	top: 100,
+});
+export const sRocky3 = style({
+	width: 1248,
+	transform: 'rotate(22deg)',
+	right: -150,
+	bottom: -315,
 });
--- src/components/section/CV/CV.tsx
+++ src/components/section/CV/CV.tsx
@@ -1,6 +1,8 @@
 import cn from 'clsx';
+import Image from 'next/image';
 
-import { sSection } from './CV.css';
+import PNG_Rocky from '@/assets/raster/rocky.png';
+import { sContent, sRocky, sRocky1, sRocky2, sRocky3, sSection } from './CV.css';
 import { CVExperience } from './CVExperience/CVExperience';
 import { CVGeneralInfo } from './CVGeneralInfo/CVGeneralInfo';
 
@@ -11,8 +13,24 @@ export interface CVProps {
 export const CV: React.FC<CVProps> = ({ className = '' }) => {
 	return (
 		<section className={cn(sSection, className)}>
-			<CVGeneralInfo />
-			<CVExperience />
+			<Rocky className={cn(sRocky1)} />
+			<Rocky className={cn(sRocky2)} />
+			<Rocky className={cn(sRocky3)} />
+
+			<div className={cn(sContent)}>
+				<CVGeneralInfo />
+				<CVExperience />
+			</div>
 		</section>
+	);
+};
+
+interface RockyProps {
+	className?: string;
+}
+
+const Rocky: React.FC<RockyProps> = ({ className }) => {
+	return (
+		<Image className={cn(sRocky, className)} src={PNG_Rocky} width={1248} height={554} alt="Blue Transparent Rock" />
 	);
 };
